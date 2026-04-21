ea938827e3af276758ed56edd3f35dd546a3da03
D-Sal22 <DioubateSalena@gmail.com>
2026-04-20 20:07:26 -0400

    removed files

--- cleaner_Class_copy.py
+++
@@ -1,8 +0,0 @@
-def clean_data(data):
-    clean_data_list = []
-
-    for row in data:
-        if row != "minutes\n":
-            clean_data.append(float(row.strip()))
-   
-    return clean_data
